a2dfbd1f7e8cb4cced9d6385f0bd4f8f2bbd87b3
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-12-27 14:31:26 +0600

    Adding default constructor and overloading the method inside the class ThreadSafeSingleton

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -4,8 +4,21 @@ public class ThreadSafeSingleton {
     private static volatile ThreadSafeSingleton instance;
     public String sharedDatabase;
 
+    private ThreadSafeSingleton() {}
+
     private ThreadSafeSingleton(String sharedDatabase) {
         this.sharedDatabase = sharedDatabase;
+    }
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton result = instance;
+        if (result != null)
+            return result;
+        synchronized(ThreadSafeSingleton.class) {
+            if (instance == null)
+                instance = new ThreadSafeSingleton();
+            return instance;
+        }
     }
 
     public static ThreadSafeSingleton getInstance(String sharedDatabase) {
